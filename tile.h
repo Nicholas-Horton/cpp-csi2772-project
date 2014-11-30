df3b52cccbda563e0bbd547ad617245c67063ca7
Julien <julien.mageau@live.ca>
2014-11-30 15:55:09 -0500

    Created tile header

--- tile.h
+++ tile.h
@@ -3,9 +3,11 @@
 
 class Tile{
 	private:
-
 	public:
-
+		bool operator==(const Tile &t);
+		virtual bool action(Player& player);
+		virtual Tile* clone();
+		ostream& operator<<(ostream &out, const Tile& tile);
 };
 
 #endif //TILE_H
