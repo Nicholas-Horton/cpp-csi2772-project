32ba38ca24c04abf8dc52f5455ca71d1c7c0db00
Nicholas Horton <Nicholas.Oryan.Horton@gmail.com>
2014-11-30 14:40:37 -0500

    Fixed player implementation to fit mein Fuhrer's demands

--- player.h
+++ player.h
@@ -14,41 +14,14 @@ class Player{
 
 		int food; //le nombre de nourritures.
 
-		Player(){
-			cart = 9;
-
-			gold = 5;
-			ruby = 0;
-			spice = 1;
-			fabric = 1;
-			jewel = 1; 
-
-			food = 10;
-		}
-
-		// retourne vrai si food > 0
-		bool canAct() const{
-			return food > 0;
-		};
+		Player();
 		
+		// retourne vrai si food > 0
+		bool canAct() const;		
 		// donne une pièce d’or à un autre joueur
-		bool pay( Player& player ){
-			if (gold > 0){
-				gold--;
-				player.gold++;
-				return true;
-			}
-			else{
-				return false;
-			}
-		};
-
+		bool pay( Player& player );
 		// reduit le nombre de nourritures de 1 si food > 0
-		void eat(){
-			if (food > 0){
-				food--;
-			}
-		};
+		void eat();
 };
 
 #endif //PLAYER_H
