4f55a0ae592ac96da51bbac61298027b32213a77
Nicholas Horton <Nicholas.Oryan.Horton@gmail.com>
2014-11-30 14:36:25 -0500

    Finished player class (in header) based on instructions.

--- player.h
+++ player.h
@@ -3,9 +3,52 @@
 
 class Player{
 	private:
-		
 	public:
+		int cart; //la capacité du joueur.
+
+		int gold; //le nombre de pièces d’or.
+		int ruby; //le nombre de rubis.
+		int spice; //le nombre de sacs d’épices.
+		int fabric; //le nombre de rouleaux de tissus.
+		int jewel; //le nombre de bijoux.
+
+		int food; //le nombre de nourritures.
+
+		Player(){
+			cart = 9;
+
+			gold = 5;
+			ruby = 0;
+			spice = 1;
+			fabric = 1;
+			jewel = 1; 
+
+			food = 10;
+		}
+
+		// retourne vrai si food > 0
+		bool canAct() const{
+			return food > 0;
+		};
 		
+		// donne une pièce d’or à un autre joueur
+		bool pay( Player& player ){
+			if (gold > 0){
+				gold--;
+				player.gold++;
+				return true;
+			}
+			else{
+				return false;
+			}
+		};
+
+		// reduit le nombre de nourritures de 1 si food > 0
+		void eat(){
+			if (food > 0){
+				food--;
+			}
+		};
 };
 
 #endif //PLAYER_H
