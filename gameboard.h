177512a71fe5a12e42b49b12cc7fd2b6f608e308
Nicholas Horton <Nicholas.Oryan.Horton@gmail.com>
2014-11-30 16:39:58 -0500

    Moved everything about gameboard into header

--- gameboard.h
+++ gameboard.h
@@ -1,22 +1,78 @@
 #ifndef GAMEBOARD_H
 #define GAMEBOARD_H
 
-template <class T, class J, int X, int Y> //T for tile, J for player, X for horizontal axis, Y for vertical axis
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+enum Move {UP, DOWN, LEFT, RIGHT};
+
+template <typename T, typename J, int X, int Y> //T for tile, J for player, X for horizontal axis, Y for vertical axis
 class GameBoard{
-  private:
+	private:
+		T **tileGrid;
+		list<J> **playersGrid;
+	public:
+		GameBoard(){
+			tileGrid = new T[Y][X];
+			playerGrid = new list<J>
+		};
 
-  public:
-		GameBoard();
+		GameBoard(const GameBoard& other){
+			tileGrid = new T[Y][X];
+			playerGrid = new list<J>[Y][X];
 
-		void add(const T& tile, int row, int col);
-		const T& getTile(int row, int col) const;
-		void getCoordinate(const T &tile, int *row, int *col) const;
-		void addPlayer(J player, int row, int col); // ajoute un joueur sur le plateau
-		void setPlayer(J player);
-		J getPlayer(const std::string& playerName);
-		const T& getTile(const std::string& playerName) const;
-		std::vector<J> getPlayers(const T& tile) const;
-		const T& move(Enum Move move, const std::string& playerName );
+			for (int i(0); i < Y; i++)
+				for (int j(0); j < X; j++){
+					tileGrid[i][j] = other.tileGrid[i][j];
+					playerGrid[i][j] = other.playerGrid[i][j];
+				}
+		};
+
+		void add(const T& tile, int row, int col){
+			tileGrid[row][col] = tile;
+		};
+
+		const T& getTile(int row, int col) const{
+			return T[row][col];
+		};
+
+		void getCoordinate(const T &tile, int *row, int *col) const{
+			for (int i(0); i < Y; i++)
+				for (int j(0); j < X; j++){
+					if (tileGrid == tile){
+						*row = i;
+						*col = i;
+						return;
+					}
+				}
+		};
+
+		void addPlayer(J player, int row, int col){
+
+		}; // ajoute un joueur sur le plateau
+
+		void setPlayer(J player){
+
+		};
+
+		J getPlayer(const std::string& playerName){
+
+		};
+
+		const T& getTile(const std::string& playerName) const{
+
+		};
+
+		std::vector<J> getPlayers(const T& tile) const{
+
+		};
+
+		const T& move(Enum Move move, const std::string& playerName ){
+
+		};
 };
 
 #endif //GAMEBOARD_H
